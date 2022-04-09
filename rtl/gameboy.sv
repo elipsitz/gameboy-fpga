50ac6084fde765b66706190ebe93c49b225e54f1
Eli Lipsitz <eli.lipsitz@gmail.com>
2022-04-09 00:45:52 -0400

    Initial effort to create gameboy module

---
+++ rtl/gameboy.sv
@@ -0,0 +1,103 @@
+`timescale 1ns/1ns
+
+/// Main GameBoy module.
+/// 
+/// Does NOT include the cartridge.
+module gameboy (
+    /// Clock (4 MHz)
+    input clk,
+    /// Synchronous reset
+    input reset,
+
+    /// Cartridge address select.
+    output logic [15:0] cart_addr,
+    /// Cartridge access enable.
+    output logic        cart_enable,
+    /// Cartridge write enable.
+    output logic        cart_write,
+    /// Cartridge data in.
+    input        [7:0]  cart_data_in,
+    /// Cartridge data out.
+    output logic [7:0]  cart_data_out
+);
+    // System Bus.
+    logic bus_write = cpu_mem_write;
+
+    // Cartridge.
+    assign cart_addr = cpu_mem_addr;
+    assign cart_write = cpu_mem_write;
+    assign cart_data_out = cpu_mem_data_out;
+
+    // CPU.
+    logic [15:0] cpu_mem_addr;
+    logic        cpu_mem_enable;
+    logic        cpu_mem_write;
+    logic [7:0]  cpu_mem_data_in;
+    logic [7:0]  cpu_mem_data_out;
+    cpu cpu (
+        .clk,
+        .reset,
+        .mem_addr (cpu_mem_addr),
+        .mem_enable (cpu_mem_enable),
+        .mem_write (cpu_mem_write),
+        .mem_data_in (cpu_mem_data_in),
+        .mem_data_out (cpu_mem_data_out)
+    );
+
+    // Work RAM (WRAM).
+    logic [7:0]  work_ram [8191:0]; // DMG: 0xC000 to 0xDFFF
+    logic        work_ram_enable;
+    logic [7:0]  work_ram_data_out;
+    logic [7:0]  work_ram_data_in = cpu_mem_data_out;
+    logic [12:0] work_ram_addr = cpu_mem_addr[12:0];
+    always_ff @(posedge clk) begin
+        if (work_ram_enable) begin
+            if (bus_write) work_ram[work_ram_addr] <= work_ram_data_in;
+            else work_ram_data_out <= work_ram[work_ram_addr];
+        end
+    end
+
+    // High RAM (HRAM).
+    logic [7:0]  high_ram [127:0]; // But only 127 bytes are accessible.
+    logic        high_ram_enable;
+    logic [7:0]  high_ram_data_out;
+    logic [7:0]  high_ram_data_in = cpu_mem_data_out;
+    logic [6:0]  high_ram_addr = cpu_mem_addr[6:0];
+    always_ff @(posedge clk) begin
+        if (high_ram_enable) begin
+            if (bus_write) high_ram[high_ram_addr] <= high_ram_data_in;
+            else high_ram_data_out <= high_ram[high_ram_addr];
+        end
+    end
+
+    // Bus multiplexer.
+    always_comb begin
+        cpu_mem_data_in = 0;
+        cart_enable = 0;
+        work_ram_enable = 0;
+        high_ram_enable = 0;
+
+        if (cpu_mem_enable) begin
+            // Cartridge ROM: 0x0000 to 0x7FFF.
+            if (cpu_mem_addr <= 16'h7FFF) begin
+                cart_enable = 1;
+                cpu_mem_data_in = cart_data_in;
+            end
+            // Cartridge RAM: 0xA000 to 0xBFFF.
+            else if (cpu_mem_addr >= 16'hA000 && cpu_mem_addr <= 16'hBFFF) begin
+                cart_enable = 1;
+                cpu_mem_data_in = cart_data_in;
+            end
+            // Work RAM: 0xC000 to 0xDFFF (and mirror at 0xE000 to 0xFDFF).
+            else if (cpu_mem_addr >= 16'hC000 && cpu_mem_addr <= 16'hFDFF) begin
+                work_ram_enable = 1;
+                cpu_mem_data_in = work_ram_data_out;
+            end
+            // High RAM: 0xFF80 to 0xFFFE.
+            else if (cpu_mem_addr >= 16'hFF80 && cpu_mem_addr <= 16'hFFFE) begin
+                high_ram_enable = 1;
+                cpu_mem_data_in = high_ram_data_out;
+            end
+        end
+    end
+endmodule
--- sim/main.cpp
+++ sim/main.cpp
@@ -5,9 +5,9 @@
 
 #include <verilated.h>
 
-#include "Vcpu.h"
+#include "Vgameboy.h"
 
-Vcpu *top;
+Vgameboy *top;
 
 vluint64_t main_time = 0;
 
@@ -35,12 +35,9 @@ int main(int argc, char** argv) {
     auto rom = read_file(argv[1]);
     uint64_t max_time = 8 * 1000000 * 20;
 
-    uint8_t memory[8192] = {0};
-    uint8_t high_memory[256] = {0};
-
-    top = new Vcpu;
+    top = new Vgameboy;
     top->reset = 1;
-    top->mem_data_in = 0;
+    top->cart_data_in = 0;
 
     while (!Verilated::gotFinish() && main_time < max_time) {
         if (main_time > 8) {
@@ -52,31 +49,18 @@ int main(int argc, char** argv) {
         }
         if (main_time % 8 == 4) {
             // Handle memory.
-            if (top->mem_enable) {
-                uint16_t address = top->mem_addr;
-                if (top->mem_write) {
-                    uint8_t data = top->mem_data_out;
-                    // printf("mem write at [%.04X] <= [%.02X]\n", address, data);
-                    if (address >= 0xC000 && address <= 0xDFFF) {
-                        memory[address - 0xC000] = data;
-                    } else if (address == 0xFF02 && data == 0x81) {
-                        // Serial Write
-                        char c = high_memory[0x01];
-                        printf("%c", c); 
-                    } else if (address >= 0xFF00 && address <= 0xFFFF) {
-                        high_memory[address - 0xFF00] = data;
-                    }
+            if (top->cart_enable) {
+                uint16_t address = top->cart_addr;
+                if (top->cart_write) {
+                    uint8_t data = top->cart_data_out;
+                    printf("cart write at [%.04X] <= [%.02X]\n", address, data);
                 } else {
                     uint8_t data = 0;
                     if (address >= 0x0000 && address <= 0x7FFF) {
                         data = rom[address];
-                    } else if (address >= 0xC000 && address <= 0xDFFF) {
-                        data = memory[address - 0xC000];
-                    } else if (address >= 0xFF00 && address <= 0xFFFF) {
-                        data = high_memory[address - 0xFF00];
                     }
-                    // printf("mem read at [%.04X] => [%.02X]\n", address, data);
-                    top->mem_data_in = data;
+                    printf("mem read at [%.04X] => [%.02X]\n", address, data);
+                    top->cart_data_in = data;
                 }
             }
         }
