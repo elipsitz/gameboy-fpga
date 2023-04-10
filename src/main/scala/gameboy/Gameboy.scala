d62553b6c0313c092b7c7f658267da2554278ef6
Eli Lipsitz <eli.lipsitz@gmail.com>
2023-04-10 16:11:30 -0500

    Add vram and oam

--- src/main/scala/gameboy/Gameboy.scala
+++ src/main/scala/gameboy/Gameboy.scala
@@ -38,6 +38,8 @@ class Gameboy extends Module {
   val phiPulse = cpu.io.tCycle === 3.U
 
   val workRam = Module(new SinglePortRam(8 * 1024)) // DMG: 0xC000 to 0xDFFF
+  val videoRam = Module(new SinglePortRam(8 * 1024)) // 0x8000 to 0x9FFF
+  val oam = Module(new SinglePortRam(160)) // 0xFE00 to 0xFE9F
 
   // Basic peripherals
   val debugSerial = Module(new DebugSerial)
@@ -76,6 +78,28 @@ class Gameboy extends Module {
   workRam.io.write := busMemWrite && phiPulse
   workRam.io.dataWrite := busDataWrite
 
+  // Video ram
+  // TODO allow PPU and OAM DMA to access this
+  // PPU locks it during pixel fetch mode (not hblank, vblank, oam search)
+  // OAM DMA locks it if reading from this region
+  // Priority: OAM DMA > PPU > CPU
+  val videoRamSelect = busAddress >= 0x8000.U && busAddress < 0xA000.U
+  videoRam.io.address := cpu.io.memAddress
+  videoRam.io.enabled := cpu.io.memEnable && videoRamSelect
+  videoRam.io.write := cpu.io.memWrite && phiPulse
+  videoRam.io.dataWrite := cpu.io.memDataOut
+
+  // Oam ram
+  // TODO allow PPU and OAM DMA to access this
+  // PPU locks it during oam dma and pixel fetch mode (not hblank, vblank)
+  // OAM DMA locks it if writing
+  // Priority: OAM DMA > PPU > CPU
+  val oamSelect = busAddress >= 0xFE00.U && busAddress < 0xFEA0.U
+  oam.io.address := cpu.io.memAddress(7, 0)
+  oam.io.enabled := cpu.io.memEnable && oamSelect
+  oam.io.write := cpu.io.memWrite && phiPulse
+  oam.io.dataWrite := cpu.io.memDataOut
+
   // Peripheral bus
   val peripherals = Seq(debugSerial.io, highRam.io, timer.io)
   val peripheralSelect = cpu.io.memAddress(15, 8) === 0xFF.U
@@ -89,11 +113,15 @@ class Gameboy extends Module {
   val peripheralDataRead = Mux1H(peripherals.map(p => (p.valid, p.dataRead)))
 
   // CPU connection to the busses
-  cpu.io.memDataIn := Mux1H(Seq(
+  // TODO bootrom
+  val cpuInputs = Seq(
     (busReadValid, busDataRead),
     (peripheralValid, peripheralDataRead),
-    (!(busReadValid || peripheralValid), 0xFF.U)
-  ))
+    (videoRamSelect, videoRam.io.dataRead),
+    (oamSelect, oam.io.dataRead),
+  )
+  val cpuInputsValid = VecInit(cpuInputs.map(_._1)).asUInt.orR
+  cpu.io.memDataIn := Mux1H(cpuInputs ++ Seq((!cpuInputsValid, 0xFF.U)))
 }
 
 object Gameboy extends App {
