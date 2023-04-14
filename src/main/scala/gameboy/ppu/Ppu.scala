af4cd1aaa9f39553c79a94725c1a9f38a172b40e
Eli Lipsitz <eli.lipsitz@gmail.com>
2023-04-14 15:02:24 -0700

    ppu: Initial background drawing (needs to be cleaned up)

--- src/main/scala/gameboy/ppu/Ppu.scala
+++ src/main/scala/gameboy/ppu/Ppu.scala
@@ -22,6 +22,43 @@ object Ppu {
   val NumScanlines = 154
   val NumTicks = 456
   val OamScanLength = 80
+  val FifoSize = 8
+}
+
+class PixelFifo[T <: Data](gen: T) extends Module {
+  val io = IO(new Bundle {
+    val reloadData = Input(Vec(8, gen))
+    val reloadEnable = Input(Bool())
+    val popEnable = Input(Bool())
+    val outData = Output(gen)
+    val outValid = Output(Bool())
+    val reloadAck = Output(Bool())
+  })
+
+  val register = Reg(Vec(8, gen))
+  val length = RegInit(0.U(4.W))
+  io.outData := register(0)
+  io.outValid := length =/= 0.U
+
+  io.reloadAck := false.B
+  when (io.reloadEnable && (length === 0.U || (io.popEnable && length === 1.U))) {
+    register := io.reloadData
+    io.reloadAck := true.B
+    length := 8.U
+  } .elsewhen (io.popEnable && length > 0.U) {
+    register := VecInit(register.slice(1, 8) ++ Seq(DontCare))
+    length := length - 1.U
+  }
+}
+
+class BgPixel extends Bundle {
+  /** Color */
+  val color = UInt(2.W)
+}
+
+object FetcherState extends ChiselEnum {
+  val id0, id1, lo0, lo1, hi0, hi1, push = Value
+  // In hi1 and push we try to push to the FIFO
 }
 
 class Ppu extends Module {
@@ -43,11 +80,11 @@ class Ppu extends Module {
   })
 
   /** Current scanline, [0, 154) */
-  val scanline = RegInit(0.U(8.W))
+  val regLy = RegInit(0.U(8.W))
   /** Current tick within scanline, [0, 456) */
   val tick = RegInit(0.U(9.W))
   /** Current number of pixels output in this scanline, [0, 160] */
-  val pixelsDrawn = RegInit(0.U(8.W))
+  val regLx = RegInit(0.U(8.W))
 
   /** $FF40 -- LCDC: LCD Control */
   val regLcdc = RegInit(0.U.asTypeOf(new RegisterLcdControl))
@@ -73,24 +110,24 @@ class Ppu extends Module {
   // Tick and scanline adjustment
   when (tick === (Ppu.NumTicks - 1).U) {
     tick := 0.U
-    pixelsDrawn := 0.U
-    when (scanline === (Ppu.NumScanlines - 1).U) {
-      scanline := 0.U
+    regLx := 0.U
+    when (regLy === (Ppu.NumScanlines - 1).U) {
+      regLy := 0.U
     } .otherwise {
-      scanline := scanline + 1.U
+      regLy := regLy + 1.U
     }
   } .otherwise {
     tick := tick + 1.U
   }
 
   // 1-hot current state wire
-  val stateVblank = scanline >= Ppu.Height.U
+  val stateVblank = regLy >= Ppu.Height.U
   val stateOamSearch = !stateVblank && (tick < Ppu.OamScanLength.U)
-  val stateDrawing = !stateVblank && (tick >= Ppu.OamScanLength.U) && (pixelsDrawn < Ppu.Width.U)
-  val stateHblank = !stateVblank && (pixelsDrawn === Ppu.Width.U)
+  val stateDrawing = !stateVblank && (tick >= Ppu.OamScanLength.U) && (regLx < Ppu.Width.U)
+  val stateHblank = !stateVblank && (regLx === Ppu.Width.U)
   io.output.hblank := stateHblank
   io.output.vblank := stateVblank
-  val lycEqualFlag = regLyc === scanline
+  val lycEqualFlag = regLyc === regLy
   io.vramEnabled := stateDrawing && regLcdc.enable
 
   // Memory-mapped register access
@@ -119,7 +156,7 @@ class Ppu extends Module {
       }
       is (0x42.U) { io.registers.dataRead := regScy }
       is (0x43.U) { io.registers.dataRead := regScx }
-      is (0x44.U) { io.registers.dataRead := scanline }
+      is (0x44.U) { io.registers.dataRead := regLy }
       is (0x45.U) { io.registers.dataRead := regLyc }
       is (0x47.U) { io.registers.dataRead := regBgp.asUInt }
       is (0x48.U) { io.registers.dataRead := regObp0.asUInt }
@@ -144,44 +181,94 @@ class Ppu extends Module {
   )).orR
   io.statIrq := !ShiftRegister(statInterrupt, 1, false.B, true.B) && statInterrupt
 
-  // Test reading from VRAM
-  val testState = RegInit(0.U(8.W))
-  val buffHi = RegInit(0.U(8.W))
-  val buffLo = RegInit(0.U(8.W))
+  // Background FIFO
+  val bgFifo = Module(new PixelFifo(new BgPixel))
+  bgFifo.io.popEnable := false.B
+  bgFifo.io.reloadEnable := false.B
+  bgFifo.io.reloadData := DontCare
 
-  val screenX = pixelsDrawn
-  val tileX = screenX(6, 3)
-  val tileY = scanline(6, 3)
-  val tileNum = Cat(tileY, tileX)
-  val tileSx = screenX(2, 0)
-  val tileSy = scanline(2, 0)
-  val tileByte = Cat(tileNum, tileSy, testState === 11.U)
-
+  // Output pixel logic
   io.output.pixel := DontCare
   io.output.valid := false.B
-  io.vramAddress := tileByte
-
-  when(stateDrawing) {
-    when(testState === 0.U) {
-      testState := 11.U
-    } .otherwise {
-      testState := testState - 1.U
-    }
-
-    when (testState === 10.U) {
-      buffHi := Reverse(io.vramDataRead)
-    }
-    when (testState === 9.U) {
-      buffLo := Reverse(io.vramDataRead)
-    }
-    when (testState <= 8.U && testState >= 1.U) {
+  when (stateDrawing) {
+    when (bgFifo.io.outValid) {
+      io.output.pixel := bgFifo.io.outData.color
+      bgFifo.io.popEnable := true.B
+      regLx := regLx + 1.U
       io.output.valid := true.B
-      io.output.pixel := Cat(buffHi(0), buffLo(0))
-      buffHi := buffHi >> 1
-      buffLo := buffLo >> 1
-      pixelsDrawn := pixelsDrawn + 1.U
     }
-  } .otherwise {
-    testState := 0.U
+  }
+
+  // Background fetch logic
+  io.vramAddress := DontCare
+  val fetcherState = RegInit(FetcherState.id0)
+  val fetcherTileId = RegInit(0.U(8.W))
+  val fetcherTileLo = RegInit(0.U(8.W))
+  val fetcherTileHi = RegInit(0.U(8.W))
+  // TODO handle vertical flip
+  val fetcherTileAddress = Cat(
+    !(regLcdc.bgTileDataArea | fetcherTileId(7)),
+    fetcherTileId,
+    (regLy + regScy)(2, 0),
+  )
+  when (stateDrawing) {
+    switch(fetcherState) {
+      // Set tilemap address
+      is (FetcherState.id0) {
+        // TODO handle window mode
+        val tileY = (regLy + regScy)(7, 3)
+        val tileX = (regLx + regScx)(7, 3)
+        io.vramAddress := Cat("b11".U(2.W), regLcdc.bgTileMapArea, tileY, tileX)
+        fetcherState := FetcherState.id1
+      }
+      // Store tilemap address
+      is (FetcherState.id1) {
+        fetcherTileId := io.vramDataRead
+        fetcherState := FetcherState.lo0
+      }
+      // Set tile low address
+      is (FetcherState.lo0) {
+        io.vramAddress := Cat(fetcherTileAddress, 0.U(1.W))
+        fetcherState := FetcherState.lo1
+      }
+      // Store tile low data
+      is (FetcherState.lo1) {
+        fetcherTileLo := io.vramDataRead
+        fetcherState := FetcherState.hi0
+      }
+      // Set tile high address
+      is (FetcherState.hi0) {
+        io.vramAddress := Cat(fetcherTileAddress, 1.U(1.W))
+        fetcherState := FetcherState.hi1
+      }
+      // Store tile high data
+      is (FetcherState.hi1) {
+        fetcherTileHi := io.vramDataRead
+        fetcherState := FetcherState.push
+        bgFifo.io.reloadEnable := true.B
+        bgFifo.io.reloadData := VecInit(
+          (0 until 8).reverse.map(i => Cat(io.vramDataRead(i), fetcherTileLo(i)).asTypeOf(new BgPixel))
+        )
+        when (bgFifo.io.reloadAck) {
+          fetcherState := FetcherState.id0
+        }
+      }
+      is (FetcherState.push) {
+        // Waiting for push... don't update fetcherState, wait here until the push is done
+        bgFifo.io.reloadEnable := true.B
+        bgFifo.io.reloadData := VecInit(
+          (0 until 8).reverse.map(i => Cat(fetcherTileHi(i), fetcherTileLo(i)).asTypeOf(new BgPixel))
+        )
+        when (bgFifo.io.reloadAck) {
+          fetcherState := FetcherState.id0
+        }
+      }
+    }
+  }
+
+  // On hblank, reset scanline registers
+  when(stateHblank) {
+    fetcherState := FetcherState.id0
+    bgFifo.reset := true.B
   }
 }
