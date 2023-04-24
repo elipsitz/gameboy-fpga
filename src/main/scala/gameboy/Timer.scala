65741f2496db106b35122242138df35868b32dbb
Eli Lipsitz <eli.lipsitz@gmail.com>
2023-04-24 11:43:48 -0700

    timer: Only reset TIMA when an increment causes it to overflow
    Previously, the overflow condition was based on (TIMA + 1) overflowing,
    regardless of whether or not TIMA would actually be incremented this
    cycle. This meant that TIMA reloaded any time it had a value of 0xFF,
    rather than waiting for the next increment.

--- src/main/scala/gameboy/Timer.scala
+++ src/main/scala/gameboy/Timer.scala
@@ -33,7 +33,7 @@ class Timer extends Module {
   val counterIncrement = fallingEdgeDetector(dividerOut && regControl.enable)
   val counterReload = WireDefault(regModulo)
   val counterPostIncrement = regCounter +& 1.U
-  val counterCarryDelay = ShiftRegister(counterPostIncrement(8), 1, false.B, true.B)
+  val counterOverflowDelay = ShiftRegister(counterIncrement && counterPostIncrement(8), 1, false.B, true.B)
   val counterWritten = WireDefault(false.B)
 
   // Priority 3/3: memory mapped write to counter
@@ -75,7 +75,7 @@ class Timer extends Module {
     }
   }
 
-  val counterOverflowed = counterCarryDelay && !counterWritten
+  val counterOverflowed = counterOverflowDelay && !counterWritten
   io.interruptRequest := counterOverflowed
   when (counterOverflowed) {
     // Priority 1/3: reload overflowing counter
