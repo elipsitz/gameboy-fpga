a6f21e72cb51e0f2bc43394709d048eeabc166b0
Eli Lipsitz <eli.lipsitz@gmail.com>
2023-07-01 01:04:36 -0500

    Add System wrapper

--- python/gameboy_ps/__main__.py
+++ python/gameboy_ps/__main__.py
@@ -6,43 +6,9 @@ Zynq PS side of the Gameboy, using the Pynq API to provide input and ROM loading
 Must be run as root.
 """
 
-import signal
-import time
-import sys
 import logging
 logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', level=logging.DEBUG)
 
-from pathlib import Path
-
-from .gameboy import Gameboy
-from . import controller
-
-# Set up Gameboy.
-gameboy = Gameboy()
-
-if len(sys.argv) < 2:
-    logging.info("Using physical cartridge")
-    gameboy.set_physical_cartridge()
-else:
-    rom_path = Path(sys.argv[1])
-    logging.info("Loading ROM from %s", rom_path)
-    gameboy.set_emulated_cartridge(rom_path)
-
-# Set up controllers.
-def controller_callback(button: controller.Button, pressed: bool) -> None:
-    gameboy.set_button(button, pressed)
-    
-controllers = [c(controller_callback) for c in controller.CONTROLLER_LISTENERS]
-
-# Start the game.
-logging.info("Initialization complete.")
-gameboy.set_paused(False)
-
-# Wait.
-try:
-    signal.pause()
-except KeyboardInterrupt:
-    pass
-
-gameboy.set_paused(True)
-gameboy.persist_ram()
+from . import system
+system = system.System()
+system.start()
---
+++ python/gameboy_ps/system.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import signal
+import time
+import logging
+
+from pathlib import Path
+from typing import Optional
+
+from .gameboy import Gameboy
+from . import controller
+
+class System:
+    def __init__(self, rom_path: Optional[Path]):
+        self.gameboy = Gameboy()
+
+        if rom_path is None:
+            logging.info("Using physical cartridge")
+            self.gameboy.set_physical_cartridge()
+        else:
+            logging.info("Loading ROM from %s", rom_path)
+            self.gameboy.set_emulated_cartridge(rom_path)
+
+        # Set up controllers.
+        def controller_callback(button: controller.Button, pressed: bool) -> None:
+            self.gameboy.set_button(button, pressed)
+    
+        controllers = [c(controller_callback) for c in controller.CONTROLLER_LISTENERS]
+
+        logging.info("Initialization complete.")
+
+    def start(self) -> None:
+        self.gameboy.set_paused(False)
+
+        # Wait.
+        try:
+            signal.pause()
+        except KeyboardInterrupt:
+            pass
+
+        self.gameboy.set_paused(True)
+        self.gameboy.persist_ram()
